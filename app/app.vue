2e7fa27e31d20659f701edd88f16c644259c788e
gdghanu-thanhdat2508 <gdschanu.datnt2508@gmail.com>
2025-08-25 18:35:03 +0700

    add

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,12 @@
 <template>
   <UApp>
+    <Analytics />
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
   </UApp>
 </template>
+
+<script setup lang="ts">
+import { Analytics } from "@vercel/analytics/nuxt";
+</script>
