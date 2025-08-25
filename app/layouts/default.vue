f2a2d5bc4e3c6ebc12a5c70f3e3c72c1af6c8d84
gdghanu-thanhdat2508 <gdschanu.datnt2508@gmail.com>
2025-08-25 18:29:18 +0700

    add

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,8 +1,13 @@
 <template>
-    <div>
-        <Header />
-        <main class="p-4">
-            <slot />
-        </main>
-    </div>
+  <div>
+    <SpeedInsights />
+    <Header />
+    <main class="p-4">
+      <slot />
+    </main>
+  </div>
 </template>
+
+<script lang="ts" setup>
+import { SpeedInsights } from "@vercel/speed-insights/nuxt";
+</script>
